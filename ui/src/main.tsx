50f3abbc4d73cfa393b46e1ec7df48c6d987239e
ai-business-hql <ai.bussiness.hql@gmail.com>
2025-04-21 21:00:54 +0800

    feat: 嵌入comfyui,添加black/light转换

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@
  * @Author: ai-business-hql ai.bussiness.hql@gmail.com
  * @Date: 2025-02-17 20:53:45
  * @LastEditors: ai-business-hql ai.bussiness.hql@gmail.com
- * @LastEditTime: 2025-04-21 19:24:02
+ * @LastEditTime: 2025-04-21 21:00:23
  * @FilePath: /comfyui_copilot/ui/src/main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -57,7 +57,7 @@ waitForDocumentBody()
       },
     });
   })
-  .then(() => {
-    app.extensionManager.setting.set('Comfy.Sidebar.Location', 'left');
-  })
+  // .then(() => {
+  //   app.extensionManager.setting.set('Comfy.Sidebar.Location', 'left');
+  // })
   ;
