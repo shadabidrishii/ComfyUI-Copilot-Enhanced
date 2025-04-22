58b90c2dc9661b1646d9c8029c633455beca2123
ai-business-hql <ai.bussiness.hql@gmail.com>
2025-04-22 10:51:23 +0800

    bug: app import

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@
  * @Author: ai-business-hql ai.bussiness.hql@gmail.com
  * @Date: 2025-02-17 20:53:45
  * @LastEditors: ai-business-hql ai.bussiness.hql@gmail.com
- * @LastEditTime: 2025-04-21 21:00:23
+ * @LastEditTime: 2025-04-22 10:50:12
  * @FilePath: /comfyui_copilot/ui/src/main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,6 +13,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { waitForApp } from "./utils/comfyapp.ts";
 import "./scoped-tailwind.css";
+import { app } from "./utils/comfyapp";
 
 const App = React.lazy(() =>
   import("./App.tsx").then(({ default: App }) => ({
